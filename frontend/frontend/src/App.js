e83afdbf93fcd39176ad3dd208fa3a1bd5927999
Lostskill <kollg.maks@gmail.com>
2023-04-11 14:54:15 +0600

    добавлено модальное окно на PostForm

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 import PostForm from './components/PostForm';
 import TodoService from './components/API/ToDoService';
 import Loader from './components/UI/Loader/Loader';
+import MyModal from './components/UI/MyModal/MyModal';
+import MyButton from './components/UI/button/MyButton';
 
 function App(){
   const [post, setPosts] = useState([]);
   const [cats, setCats] = useState([]);
-  const [isPostsLoading , setIsPostsLoading] = useState(false);
+  const [isPostsLoading, setIsPostsLoading] = useState(false);
+  const [modal, setModal] = useState(false);
 
   useEffect(() => {
     fetchList()
@@ -25,7 +28,13 @@ function App(){
 
   return (
     <div>
-      <PostForm  value={cats} option={cats} defaultValue="Категория"  />
+      <MyButton style={{marginTop:30 }} onClick={() => setModal(true)}>
+        Создать Пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal} >
+          <PostForm value={cats} option={cats} defaultValue="Категория" />
+      </MyModal>
+      
       {isPostsLoading 
       ? <div style={{display:'flex', justifyContent:'center', marginTop:50}}><Loader/></div>
       :<TodoList post={post} title='Список Постов ' />
